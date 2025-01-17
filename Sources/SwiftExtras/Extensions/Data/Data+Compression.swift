17690635c7b09fa80a82c24af5a7ce1b9cacadac
Wesley de Groot <email@wesleydegroot.nl>
2025-01-17 13:07:38 +0100

    Added new functions

--- Sources/SwiftExtras/Extensions/Data/Data+Compression.swift
+++ Sources/SwiftExtras/Extensions/Data/Data+Compression.swift
@@ -15,22 +15,22 @@ import Foundation
 import Compression
 #endif
 
-public extension Data {
+extension Data {
     /// Data as hexidecimal string
-    var hexString: String {
+    public var hexString: String {
         return self.map({
             return String(format: "%02hhx", $0)
         }).joined()
     }
 
     /// Data as string (utf8)
-    var stringValue: String? {
+    public var stringValue: String? {
         return String.init(data: self, encoding: .utf8)
     }
 }
 
 #if canImport(Compression)
-public extension Data {
+extension Data {
     /// Config (for (de)compression)
     fileprivate typealias Config = (
         operation: compression_stream_operation,
@@ -103,8 +103,8 @@ public extension Data {
     /// Compresses the data using the zlib deflate algorithm.
     /// - returns: raw deflated data according to [RFC-1951](https://tools.ietf.org/html/rfc1951).
     /// - note: Fixed at compression level 5 (best trade off between speed and time)
-    func deflate() -> Data? {
-        return self.withUnsafeBytesData { (sourcePtr: UnsafePointer<UInt8>) -> Data? in
+    public func deflate() -> Data? {
+        return self.withUnsafeBytes { (sourcePtr: UnsafePointer<UInt8>) -> Data? in
             let configuration = (
                 operation: COMPRESSION_STREAM_ENCODE,
                 algorithm: COMPRESSION_ZLIB
@@ -122,8 +122,8 @@ public extension Data {
     /// - note: Self is expected to be a raw deflate, \
     /// stream according to [RFC-1951](https://tools.ietf.org/html/rfc1951).
     /// - returns: uncompressed data
-    func inflate() -> Data? {
-        return self.withUnsafeBytesData { (sourcePtr: UnsafePointer<UInt8>) -> Data? in
+    public func inflate() -> Data? {
+        return self.withUnsafeBytes { (sourcePtr: UnsafePointer<UInt8>) -> Data? in
             let configuration = (
                 operation: COMPRESSION_STREAM_DECODE,
                 algorithm: COMPRESSION_ZLIB
@@ -135,26 +135,6 @@ public extension Data {
                 sourceSize: count
             )
         }
-    }
-
-    /// withUnsafeBytes
-    /// - Parameter body: Unsafe pointer
-    /// - Throws: ResultType
-    /// - Returns: ResultType
-    func withUnsafeBytesData<ResultType, ContentType>(
-        _ body: (UnsafePointer<ContentType>) throws -> ResultType
-    ) rethrows -> ResultType {
-        return try self.withUnsafeBytes({ (rawBufferPointer: UnsafeRawBufferPointer) -> ResultType in
-            guard let baseAddress = try body(
-                rawBufferPointer.bindMemory(
-                    to: ContentType.self
-                ).baseAddress else {
-                    fatalError("Failed to bind memory")
-                }
-
-                return baseAddress
-            )
-        })
     }
 }
 #endif
