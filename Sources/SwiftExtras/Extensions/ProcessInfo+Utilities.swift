5026e58c4af8d49221f0734682c3baf98975e254
Wesley de Groot <email@wesleydegroot.nl>
2025-02-22 18:11:54 +0100

    Fix build error

--- Sources/SwiftExtras/Extensions/ProcessInfo+Utilities.swift
+++ Sources/SwiftExtras/Extensions/ProcessInfo+Utilities.swift
@@ -13,29 +13,47 @@ import Foundation
 
 public extension ProcessInfo {
     /// Detects if running in Xcode SwiftUI Preview mode
-    static var isXcodePreview: Bool { processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1" }
-
-    /// Detects if UI Tests are running
-    static var isUITesting: Bool { processInfo.arguments.contains("UI_TESTING") }
-
-    /// Detects if Unit Tests are running
-    static var isUnitTesting: Bool { NSClassFromString("XCTestCase") != nil }
-
-    /// Detects if Low Power Mode is enabled
-    static var isLowPowerModeActive: Bool { processInfo.isLowPowerModeEnabled }
-
-    /// Returns the current system uptime in seconds
-    static var currentSystemUptime: Double { processInfo.systemUptime }
-
-    /// Returns the system uptime in days (rounded to 2 decimals)
-    static var systemUptimeInDays: Double {
-        let days = currentSystemUptime / 86400 // 1 day = 86,400 seconds
-        return round(days * 100) / 100
+    static var isXcodePreview: Bool {
+#if DEBUG
+        processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
+#else
+        false
+#endif
     }
 
-    /// Detects if running on Mac Catalyst
-    static var isRunningOnMacCatalyst: Bool { processInfo.isMacCatalystApp }
+    /// Detects if UI Tests are running
+    static var isUITesting: Bool {
+#if DEBUG
+        processInfo.arguments.contains("UI_TESTING")
+#else
+        false
+#endif
+    }
+
+    /// Detects if Unit Tests are running
+    static var isUnitTesting: Bool {
+#if DEBUG
+        NSClassFromString("XCTestCase") != nil
+#else
+        false
+#endif
+}
+
+    /// Detects if Low Power Mode is enabled
+    static var isLowPowerModeActive: Bool {
+#if os(iOS) || os(watchOS)
+        processInfo.isLowPowerModeEnabled
+#else
+        false
+#endif
+    }
 
     /// Detects if running an iOS app on Mac
-    static var isRunningiOSAppOnMac: Bool { processInfo.isiOSAppOnMac }
+    static var isRunningiOSAppOnMac: Bool {
+#if os(iOS)
+        processInfo.isiOSAppOnMac
+#else
+        false
+#endif
+    }
 }
