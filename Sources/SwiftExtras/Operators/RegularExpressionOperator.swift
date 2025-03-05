edc25f7407e85268436f864e52c5f3f1d1f0f4ca
Wesley de Groot <email@wesleydegroot.nl>
2025-03-05 17:12:36 +0100

    Add RegularExpressionOperator

---
+++ Sources/SwiftExtras/Operators/RegularExpressionOperator.swift
@@ -0,0 +1,41 @@
+//
+//  RegularExpressionOperator.swift
+//  SwiftExtras
+//
+//  Created by Wesley de Groot on 2025-03-05.
+//  https://wesleydegroot.nl
+//
+//  https://github.com/0xWDG/SwiftExtras
+//  MIT License
+//
+
+import Foundation
+
+///  Regular expression infix operator (Matching)
+infix operator =~
+
+/// Regular expression infix operator (Matching)
+/// - Parameters:
+///   - string: String to be checked
+///   - regex: against regular expression
+/// - Returns: true if found, otherwise false
+public func =~ (string: String, regex: String) -> Bool {
+    return string.range(
+        of: regex,
+        options: .regularExpression,
+        range: nil,
+        locale: nil
+    ) != nil
+}
+
+///  Regular expression infix operator (inverse matching)
+infix operator !~
+
+/// Regular expression infix operator (inverse matching)
+/// - Parameters:
+///   - string: String to be checked
+///   - regex: against regular expression
+/// - Returns: true if not found, otherwise false
+public func !~ (source: String, pattern: String) -> Bool {
+    return !(source =~ pattern)
+}
