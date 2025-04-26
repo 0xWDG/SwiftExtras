9165a652a65af47fd0cde0e8765c7c80c5307c3f
Wesley de Groot <email@wesleydegroot.nl>
2025-04-26 13:32:11 +0200

    Add Logger init

---
+++ Sources/SwiftExtras/Extensions/Logger+.swift
@@ -0,0 +1,29 @@
+//
+//  Logger+.swift
+//  SwiftExtras
+//
+//  Created by Wesley de Groot on 2025-04-26.
+//  https://wesleydegroot.nl
+//
+//  https://github.com/0xWDG/SwiftExtras
+//  MIT License
+//
+
+#if canImport(OSLog)
+import OSLog
+
+extension Logger {
+    /// Start a logger with default values
+    /// subsystem: Bundle.main.bundleIdentifier
+    /// catefory: #function (or class when called from a class)
+    ///
+    /// - Parameter default: Set this to whatever you want
+    /// - Parameter category: #function
+    public init(default: Any = true, category: StaticString = #function) {
+        self.init(
+            subsystem: Bundle.main.bundleIdentifier ?? "nl.wesleydegroot",
+            category: String(describing: category)
+        )
+    }
+}
+#endif
