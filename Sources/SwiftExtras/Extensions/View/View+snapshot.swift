83d441b5293333a1fa244e40c5c68636be7a8ebe
Wesley de Groot <email@wesleydegroot.nl>
2025-09-13 13:34:35 +0200

    Lint fixes

--- Sources/SwiftExtras/Extensions/View/View+snapshot.swift
+++ Sources/SwiftExtras/Extensions/View/View+snapshot.swift
@@ -12,7 +12,6 @@
 #if canImport(SwiftUI)
 import SwiftUI
 
-
 extension View {
     /// Captures a snapshot of the current view and returns it as a `PlatformImage`.
     /// - Parameter size: The desired size of the snapshot. Defaults to the device's screen size.
@@ -34,7 +33,9 @@ extension View {
         let hostingView = NSHostingView(rootView: self)
         hostingView.frame = CGRect(origin: .zero, size: size)
 
-        let bitmapRep = hostingView.bitmapImageRepForCachingDisplay(in: hostingView.bounds)!
+        guard let bitmapRep = hostingView.bitmapImageRepForCachingDisplay(in: hostingView.bounds) else {
+            return PlatformImage()
+        }
         hostingView.cacheDisplay(in: hostingView.bounds, to: bitmapRep)
 
         let image = NSImage(size: size)
@@ -57,7 +58,10 @@ extension View {
 #if targetEnvironment(simulator)
         // We will write the snapshot to the desktop directory in the simulator for easy access.
         let image = self.snapshot(size: size)
-        let desktopURL = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first!
+        guard let desktopURL = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first else {
+            return self
+        }
+
         let fileURL = desktopURL.appendingPathComponent("\(AppInfo.appName)_\(name)_\(size.width)x\(size.height).png")
 
         try? image.pngData()?.write(to: fileURL)
