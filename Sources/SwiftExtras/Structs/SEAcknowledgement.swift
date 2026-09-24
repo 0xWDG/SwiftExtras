eb8f9df6e2c1fe3e6544c295a72ad815315f49c4
Wesley de Groot <email@wesleydegroot.nl>
2026-09-24 15:14:20 +0200

    Introduce `PreferenceKit`

--- Sources/SwiftExtras/Structs/SEAcknowledgement.swift
+++ Sources/SwiftExtras/Structs/SEAcknowledgement.swift
@@ -13,6 +13,6 @@ import Foundation
 
 import PreferenceKit
 
-@available(*, deprecated, renamed: "Acknowledgement", message: "Use Acknowledgement instead")
 /// Deprecated typealias for `Acknowledgement`. Use `Acknowledgement` instead.
+@available(*, deprecated, renamed: "Acknowledgement", message: "Use Acknowledgement instead")
 public typealias SEAcknowledgement = Acknowledgement
